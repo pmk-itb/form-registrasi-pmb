03d8e7686e77cf3e1d32cb1cdbb15dcbee2ccc24
Samuel <samnap11@gmail.com>
2021-05-21 14:47:56 +0700

    build: custom 404 routing

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,8 +1,13 @@
 import Form from '../views/Form.vue';
 import ThankYou from '../views/ThankYou.vue';
 import { createRouter, createWebHistory } from 'vue-router';
+import NotFound from '../views/NotFound.vue';
 
 const routes = [
+  {
+    path: '/:pathMatch(.*)',
+    component: NotFound,
+  },
   {
     path: '/',
     component: Form,
