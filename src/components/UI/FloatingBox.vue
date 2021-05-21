9a8f81b0e49fb049bc55b70fea8b85e85d772b38
Samuel <samnap11@gmail.com>
2021-05-21 22:33:14 +0700

    feat: add reusable floating box component

---
+++ src/components/UI/FloatingBox.vue
@@ -0,0 +1,7 @@
+<template>
+  <div
+    class="flex flex-col py-20 space-y-6 items-center justify-center bg-gradient-to-br from-white to-gray-100 filter drop-shadow-lg backdrop-filter backdrop-blur-2xl rounded-lg w-8/12 h-1/2 max-w-screen-xl"
+  >
+    <slot></slot>
+  </div>
+</template>
