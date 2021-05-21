102a3ce5c14b68faaaeb03e83c0c00ac7894d9b5
Samuel <samnap11@gmail.com>
2021-05-21 22:33:40 +0700

    feat: add main body of thank you page

--- src/views/ThankYou.vue
+++ src/views/ThankYou.vue
@@ -1,3 +1,21 @@
 <template>
-  <h1>Thank You</h1>
+  <div class="flex flex-col items-center">
+    <!-- Header component begin -->
+
+    <!-- Header component end -->
+    <!-- Floating Box component begin -->
+    <FloatingBox>
+      <img src="../assets/thankyou-phone.png" />
+      <h2 class="font-bold font-roboto text-4xl text-blue-thank-you">Thank You!</h2>
+      <div class="font-normal font-roboto text-sm">Your data has been saved at Intern PMK ITB</div>
+    </FloatingBox>
+    <!-- Floating Box component end -->
+    <!-- Contact Us component begin -->
+
+    <!-- Contact Us component end -->
+  </div>
 </template>
+
+<script setup lang="ts">
+import FloatingBox from '../components/UI/FloatingBox.vue';
+</script>
