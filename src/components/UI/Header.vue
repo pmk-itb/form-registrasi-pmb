0abf93ef8758d72d17638cd287889e6ee67e286d
dethaa <sharonmarbun12@gmail.com>
2021-05-29 16:43:21 +0700

    feat: add Header

---
+++ src/components/UI/Header.vue
@@ -0,0 +1,57 @@
+<template>
+  <h1 class="title">PMB PMK ITB 2021</h1>
+  <img class="logo" src="https://bit.ly/fcc-relaxing-cat" alt="Logo PMB PMK ITB 2021" />
+  <div class="circle1"></div>
+  <div class="circle2"></div>
+</template>
+
+<style>
+.title {
+  position: absolute;
+  width: 427px;
+  height: 56px;
+  left: 506px;
+  top: 90px;
+
+  font-family: Roboto;
+  font-style: normal;
+  font-weight: bold;
+  font-size: 36px;
+  line-height: 56px;
+  text-align: center;
+
+  color: #4d76b7;
+}
+
+.logo {
+  position: absolute;
+  width: 60px;
+  height: 60px;
+  left: 675px;
+  top: 50px;
+
+  background: #c4c4c4;
+}
+
+.circle1 {
+  position: absolute;
+  width: 350px;
+  height: 350px;
+  left: 1000px;
+  top: 350px;
+  border-radius: 50%;
+
+  background: linear-gradient(223.16deg, #ffdc95 24.25%, #afceff 85.77%, #ffe5af 85.77%);
+}
+
+.circle2 {
+  position: absolute;
+  width: 120px;
+  height: 120px;
+  left: 800px;
+  top: 540px;
+  border-radius: 50%;
+
+  background: linear-gradient(61.74deg, #4d76b7 1.11%, #95beff 68.71%);
+}
+</style>
--- src/views/ThankYou.vue
+++ src/views/ThankYou.vue
@@ -1,3 +1,9 @@
 <template>
+  <Header />
   <h1>Thank You</h1>
 </template>
+
+<script setup lang="ts">
+import { defineComponent } from 'vue';
+import Header from '../components/UI/Header.vue';
+</script>
