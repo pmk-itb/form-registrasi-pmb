531216fe195538cf49b9264335552a73746647b5
Samuel <53994055+samnap11@users.noreply.github.com>
2021-06-07 20:27:12 +0700

    feat: add main page template (#10)
    * fix: page title
    
    * feat: add main page's template

--- src/views/Form.vue
+++ src/views/Form.vue
@@ -1,3 +1,13 @@
 <template>
-  <h1>Form</h1>
+  <div id="form-page" class="flex flex-col items-center space-y-10 md:space-y-20">
+    <Header />
+    <FloatingBox></FloatingBox>
+    <Footer />
+  </div>
 </template>
+
+<script setup lang="ts">
+import Header from '../components/UI/Header.vue';
+import Footer from '../components/UI/Footer.vue';
+import FloatingBox from '../components/UI/FloatingBox.vue';
+</script>
