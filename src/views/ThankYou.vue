be0ec5c657be6bb84f97665382590e3fc301b612
Samuel <samnap11@gmail.com>
2021-06-16 15:26:20 +0700

    refactor: move padding and width out of floating box component

--- src/views/ThankYou.vue
+++ src/views/ThankYou.vue
@@ -4,7 +4,7 @@
     <Header />
     <!-- Header component end -->
     <!-- Floating Box component begin -->
-    <FloatingBox>
+    <FloatingBox class="py-10 md:py-20 md:w-8/12">
       <img class="w-48 md:w-auto" src="../assets/thank-you-phone.svg" />
       <h2 class="font-bold font-roboto text-xl md:text-4xl text-blue-primary">Thank You!</h2>
       <div class="font-normal font-roboto text-xs md:text-sm text-gray-secondary">
