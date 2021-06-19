76960067dd6dbd7a0971621741d182489aa86a49
Samuel <samnap11@gmail.com>
2021-06-19 14:18:48 +0700

    feat: add form's pagination component

--- src/views/Form.vue
+++ src/views/Form.vue
@@ -1,7 +1,10 @@
 <template>
   <div id="form-page" class="flex flex-col items-center space-y-10 md:space-y-20">
     <Header />
-    <FloatingBox></FloatingBox>
+    <FloatingBox class="pt-7 pb-11 md:w-10/12">
+      <PageIndicator />
+      <FormNavigateButtonGroup />
+    </FloatingBox>
     <Footer />
   </div>
 </template>
@@ -10,4 +13,6 @@
 import Header from '../components/UI/Header.vue';
 import Footer from '../components/UI/Footer.vue';
 import FloatingBox from '../components/UI/FloatingBox.vue';
+import PageIndicator from '../components/Pagination/PageIndicator.vue';
+import FormNavigateButtonGroup from '../components/Pagination/FormNavigateButtonGroup.vue';
 </script>
