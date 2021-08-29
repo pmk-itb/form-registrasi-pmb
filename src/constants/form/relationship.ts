1a4ea016122f48b493bb839b2d739f3895a0cfda
Samuel <53994055+samnap11@users.noreply.github.com>
2021-08-29 15:16:44 +0700

    feat: update relationship options (#14)

---
+++ src/constants/form/relationship.ts
@@ -0,0 +1,12 @@
+export const relationships = [
+  { id: 'AYAH', name: 'Ayah' },
+  { id: 'IBU', name: 'Ibu' },
+  { id: 'WALI', name: 'Wali' },
+  { id: 'KAKAK', name: 'Kakak' },
+  { id: 'ADIK', name: 'Adik' },
+  { id: 'BIBI', name: 'Bibi' },
+  { id: 'PAMAN', name: 'Paman' },
+  { id: 'KAKEK', name: 'Kakek' },
+  { id: 'NENEK', name: 'Nenek' },
+  { id: 'OTHER', name: 'Lainnya' },
+];
