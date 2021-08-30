b2ddd119caad383ccda3e458c2077270bee48501
Samuel <samnap11@gmail.com>
2021-08-30 13:52:51 +0700

    feat: add API client call function for submitting the form

---
+++ src/lib/form/submitForm.ts
@@ -0,0 +1,31 @@
+import client from '../apiClient';
+import { SubmitResponse } from './types/SubmitResponse';
+
+type FormSubmitData = {
+  nim: string;
+  name: string;
+  nickname: string;
+  majorId: number;
+  gender: 'MALE' | 'FEMALE';
+  birthDate: string;
+  line: string;
+  phone: string;
+  email: string;
+  originProvince: string;
+  originCity: string;
+  originSchool: string;
+  originChurch: string;
+  parentPhone: string;
+  parentRelationship: 'AYAH' | 'IBU' | 'WALI' | 'KAKAK' | 'ADIK' | 'BIBI' | 'PAMAN' | 'KAKEK' | 'NENEK' | 'OTHER';
+  discipleshipId: number;
+};
+
+const submitForm = (data: FormSubmitData): Promise<SubmitResponse> => {
+  return client
+    .post('/members', {
+      json: data,
+    })
+    .json();
+};
+
+export default submitForm;
