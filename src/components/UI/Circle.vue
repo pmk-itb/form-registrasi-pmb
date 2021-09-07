88686569e8668ef5f56efb6a77554e78da5da867
dethaa <sharonmarbun12@gmail.com>
2021-09-07 15:19:32 +0700

    style: slight color and size changes

--- src/components/UI/Circle.vue
+++ src/components/UI/Circle.vue
@@ -10,7 +10,8 @@
   width: 21rem;
   height: 21rem;
   border-radius: 50%;
-  background: linear-gradient(223.16deg, #ffdc95 24.25%, #afceff 85.77%, #ffe5af 85.77%);
+  /* background: linear-gradient(223.16deg, #ffdc95 24.25%, #afceff 85.77%, #ffe5af 85.77%); */
+  background: linear-gradient(223.16deg, #8ce4d1 24.25%, #afceff 85.77%, #c3f1d8 85.77%);
 }
 
 .circle2 {
--- src/components/UI/Header.vue
+++ src/components/UI/Header.vue
@@ -1,7 +1,7 @@
 <template>
   <header class="flex flex-col w-screen relative overflow-hidden">
     <div class="flex flex-col mx-auto w-full justify-center items-center space-y-5 max-w-screen-xl z-10">
-      <img class="h-14 w-14 mt-16 md:mt-24" :src="logo" alt="Logo PMB PMK ITB 2021" />
+      <img class="h-auto w-14 mt-16 md:mt-24" :src="logo" alt="Logo PMB PMK ITB 2021" />
       <h1 class="font-roboto font-bold text-2xl text-blue-primary md:text-5xl md:leading-[56px]">PMB PMK ITB 2021</h1>
     </div>
   </header>
